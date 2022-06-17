b5376286a87c07adb92d3c766ce527f20556f00c
Ollin Gomez <tonatiuh.ollin.gomez@gmail.com>
2022-06-17 15:10:46 -0500

    Se realiza el cambio de h2

--- salundo/script.js
+++ salundo/script.js
@@ -1,1 +1,18 @@
 console.log("Se nota que todabia me extrañas");
+
+setInterval(cambiarSaludo, 2000);
+
+function cambiarSaludo() {
+  //console.log("Me parece una falta de respecto");
+  // Estoy asignando a una variable el valor de h2
+  let frase = document.getElementsByTagName("h2")[1].innerHTML;
+
+  if (document.getElementsByTagName("h2")[1].innerHTML == "Ganando como siempre") {
+    //frase = "Impongo moda para las niñas!";
+    document.getElementsByTagName("h2")[1].innerHTML = "Impongo moda para las niñas!";
+  } else {
+    //frase = "Ganando como siempre";
+    document.getElementsByTagName("h2")[1].innerHTML = "Ganando como siempre";
+  }
+  console.log("frase :" + frase);
+}
