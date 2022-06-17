4b2d8be84e4a322ed1c5e852b36879622123a946
Ollin Gomez <tonatiuh.ollin.gomez@gmail.com>
2022-06-17 15:17:43 -0500

    Se hutili innerHTML para h2

--- salundo/script.js
+++ salundo/script.js
@@ -2,17 +2,21 @@ console.log("Se nota que todabia me extrañas");
 
 setInterval(cambiarSaludo, 2000);
 
+/**
+ * Cambia la frase en el titulo h2
+ */
+
 function cambiarSaludo() {
   //console.log("Me parece una falta de respecto");
   // Estoy asignando a una variable el valor de h2
-  let frase = document.getElementsByTagName("h2")[1].innerHTML;
+  let frase = document.getElementsByTagName("h2")[1];
 
-  if (document.getElementsByTagName("h2")[1].innerHTML == "Ganando como siempre") {
+  if (frase.innerHTML == "Ganando como siempre") {
     //frase = "Impongo moda para las niñas!";
-    document.getElementsByTagName("h2")[1].innerHTML = "Impongo moda para las niñas!";
+    frase.innerHTML = "¡Impongo moda para las niñas!";
   } else {
     //frase = "Ganando como siempre";
-    document.getElementsByTagName("h2")[1].innerHTML = "Ganando como siempre";
+        frase.innerHTML = "¡Ganando como siempre!";
   }
   console.log("frase :" + frase);
 }
